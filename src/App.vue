a21789679150002e1b9b2d08247f82d60bf14cc8
HKhanhBang <dohoangbangkhanh@gmail.com>
2024-05-17 12:47:38 +0700

    Created MainLayout.vue for layout

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,21 @@
-<script setup></script>
+<script setup>
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
+import MainLayout from './components/MainLayout.vue'
+</script>
 
 <template>
-  <RouterView></RouterView>
+  <MainLayout>
+    <template #header>
+      <Header></Header>
+    </template>
+    <template #default>
+      <RouterView></RouterView>
+    </template>
+    <template #footer>
+      <Footer></Footer>
+    </template>
+  </MainLayout>
 </template>
 
 <style scoped></style>
---
+++ src/components/MainLayout.vue
@@ -0,0 +1,25 @@
+<script setup></script>
+
+<template>
+  <header>
+    <slot name="header"></slot>
+  </header>
+  <main>
+    <slot></slot>
+  </main>
+  <footer>
+    <slot name="footer"></slot>
+  </footer>
+</template>
+
+<style scoped>
+header,
+footer {
+  background-color: var(--primary-color);
+  min-height: 64px;
+}
+
+main {
+  min-height: calc(100vh - 64px * 2);
+}
+</style>
